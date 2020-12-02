f29789ca48f56b790a5906a8946a0381e738102a
Thomas Bullock <motbollox@gmail.com>
2020-12-02 20:32:58 +1100

    update db service

--- config/index.js
+++ config/index.js
@@ -1,12 +1,7 @@
 const config = {
-  db: `mongodb://${process.env.DB_USER}:${encodeURIComponent(
+  db: `mongodb+srv://${process.env.DB_USER}:${encodeURIComponent(
     process.env.DB_PASSWORD
   )}@${process.env.DB_URL}`,
-  // options: {
-  //   keepAlive: 300000,
-  //   connectTimeoutMS: 300000,
-  //   useMongoClient: true
-  // }
   secret: process.env.SECRET,
   google: {
     clientID: process.env.GOOGLE_CLIENT_ID,
